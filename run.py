3b060f57002b0543cdd5c048457c024fae7b6a57
mani6027 <23162573+Mani6027@users.noreply.github.com>
2025-02-23 00:54:07 +0530

    :zap: Add process-excel view function

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,26 @@ def health_check():
     """
     return jsonify({"status": "ok"})
 
+
+@app.route('/process-excel', methods=['POST'])
+def process_excel():
+    """
+    Process Excel endpoint
+    """
+    if ("file" not in request.files) or ("instruction" not in request.form):
+        # TODO: raise valid exception here.
+        return jsonify({"error": "No file uploaded"}), 400
+    
+    file = request.files['file']
+    instruction = request.form['instruction']
+    
+    if file.filename == '':
+        # TODO: raise valid exception here.
+        return jsonify({"error": "No file uploaded"}), 400
+    
+    # call the function to process the excel file
+
+    return jsonify({"status": "ok"})
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
