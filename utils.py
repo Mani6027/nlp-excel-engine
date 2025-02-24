83c36ab3b7fe69af04c8f496781e6bb9cedf7b9a
mani6027 <23162573+Mani6027@users.noreply.github.com>
2025-02-24 23:57:07 +0530

    remove unused prompt

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ from pydantic import BaseModel, Field, model_validator
 from config import logger
 from constants import ErrorCodes
 from custom_exceptions import InvalidParameters, InvalidInstruction, InvalidFile
-from system_prompt import INITIAL_PROMPT, EXCEL_PARAM_EXTRACTION_PROMPT, EXCEL_PARAM_EXTRACTION_PROMPT_V2, OBJ_PROMPT_V2
+from system_prompt import OBJECTIVE_PROMPT, EXCEL_PARAM_EXTRACTION_PROMPT
 
 
 # create a schema for parameters checking from instructions
@@ -73,7 +73,7 @@ def extract_params_from_instructions(excel_metadata, instructions: str) -> dict:
         "max_output_tokens": 8192,
         "response_mime_type": "application/json",
     }
-    system_prompt = OBJ_PROMPT_V2.format(excel_metadata=excel_metadata) + EXCEL_PARAM_EXTRACTION_PROMPT_V2
+    system_prompt = OBJECTIVE_PROMPT.format(excel_metadata=excel_metadata) + EXCEL_PARAM_EXTRACTION_PROMPT
     model = genai.GenerativeModel(
         "gemini-2.0-flash",
         generation_config=generation_config,
